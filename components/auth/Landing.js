b6f84e3471eb65e3fe118358d490e9ebc36134bc
Andrew Dillon <majik895@gmail.com>
2021-01-03 14:04:29 -0500

    Add files via upload
    first commit

---
+++ components/auth/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+export default function Landing() {
+    return (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+            <Button 
+            title='Register'
+            onPress={() => NavigationPreloadManager.navigate('Register')} />
+            <Button 
+            title='Login'
+            onPress={() => NavigationPreloadManager.navigate('Login')} />
+        </View>
+    )
+}
